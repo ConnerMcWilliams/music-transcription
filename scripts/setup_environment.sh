756b9d3c6b566c6833829d55cee83606c8dd5c9d
Conner McWilliams <connermcwilliams16@gmail.com>
2026-04-01 13:09:28 -0400

    Created two bash scripts: 1. Setup environment: This downloads and sets up miniconda3. Then, it creates and activates the environment. 2. Rough outline of the cache dataset script. Directories need to be updated.

---
+++ scripts/setup_environment.sh
@@ -0,0 +1,11 @@
+MINI_DIR=$(pwd)/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh -b -p $MINI_DIR
+
+source $MINI_DIR/etc/profile.d/conda.sh
+conda init bash
+
+exec bash
+
+conda env create -f environment.yml
+conda activate music-transcriber
