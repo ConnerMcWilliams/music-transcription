01aa6ef29664083d44f53eba753ff751b4d33b85
Conner McWilliams <connermcwilliams16@gmail.com>
2026-04-01 13:17:14 -0400

    update setup script with miniconda existence check

--- scripts/setup_environment.sh
+++ scripts/setup_environment.sh
@@ -1,5 +1,11 @@
-MINI_DIR=$(pwd)/miniconda3
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+MINI_DIR=$(pwd)../miniconda3
+
+if test -f "$MINI_DIR"; then
+    echo "$MINI_DIR exists"
+else 
+    echo "$MINI_DIR does not exist. Downloading..."    
+    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+fi
 bash Miniconda3-latest-Linux-x86_64.sh -b -p $MINI_DIR
 
 source $MINI_DIR/etc/profile.d/conda.sh
