a6daba44b9085ee8a39cb665ed42a5ae5a94262d
Conner McWilliams <connermcwilliams16@gmail.com>
2026-04-13 08:33:25 -0400

    go go go go

--- scripts/TRAIN.sh
+++ scripts/TRAIN.sh
@@ -6,10 +6,10 @@ CURRENT_DIR=$(pwd)
 DATASET_DIR=$CURRENT_DIR/../dataset/corpus/MAESTRO-V3
 
 LIST_DIR=$DATASET_DIR/list
-FEATURE_DIR=$DATASET_DIR/feature
+FEATURE_DIR=$DATASET_DIR/feature/spec
 LABEL_DIR=$DATASET_DIR/label
 MIDI_DIR=$DATASET_DIR/midi
-NORM_DIR=$DATASET_DIR/norm
+NORM_DIR=$DATASET_DIR/midi/norm
 
 CHECKPOINT_DIR=$CURRENT_DIR/../checkpoints
 
@@ -21,4 +21,9 @@ NGPUS=${NGPUS:-1}
 
 torchrun --nproc_per_node=$NGPUS \
     $CURRENT_DIR/../experiment/refine_experiment.py \
-    --seed          0
+    --list_dir       $LIST_DIR \
+    --feature_dir    $FEATURE_DIR \
+    --midi_dir       $MIDI_DIR \
+    --midi_cache_dir $NORM_DIR \
+    --checkpoint_dir $CHECKPOINT_DIR \
+    --seed           0
